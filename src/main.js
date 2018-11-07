0d4a55ec6bf7feed2f1a252061f4595787cbdfd4
Aziz Haddad <aziz-haddad@users.noreply.github.com>
2018-11-07 11:24:32 +0100

    add reselect to midgard core and expose createSelector function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createSelector } from 'reselect';
 import http from './http/main';
 import oauth from './oauth/main';
 
 const redux = {
   createStore,
+  createSelector,
   combineReducers,
   applyMiddleware,
   compose
