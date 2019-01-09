977d2338fd934b3a7c65e56d11ec4ceb3ca89ec7
Alex Percsi <alex.percsi@humanitec.com>
2019-01-09 16:19:37 +0100

    JSDoc

--- src/logger/LogEntry.js
+++ src/logger/LogEntry.js
@@ -1,9 +1,26 @@
 import LOG_LEVELS from './LogLevels';
 
+/**
+ * Checks if the value passed as an argument is a valid log level as defined in ./LogLevels
+ * @param {string} logLevel the logLevel string to be validated
+ * @returns {boolean} true if the logLevel is valid, false otherwise
+ */
 const logLevelIsValid = (logLevel) => {
   return !!LOG_LEVELS[logLevel];
 };
+
+/**
+ * 
+ * @export
+ * @class LogEntry Data model for representing a log entry to be sent to the server
+ */
 export default class LogEntry {
+  /**
+   *Creates an instance of LogEntry.
+   * @param {string} message The message to send to the server
+   * @param {LogLevel} [logLevel=LOG_LEVELS.LOG] A log level to associate the message with
+   * @memberof LogEntry
+   */
   constructor(message, logLevel = LOG_LEVELS.LOG) {
     this.message = message;
     if (!logLevelIsValid(logLevel)) {
--- src/logger/main.js
+++ src/logger/main.js
@@ -2,6 +2,11 @@ import { request } from '../http/main';
 import LogEntry from './LogEntry';
 import LOG_LEVELS from './LogLevels';
 
+/**
+ * 
+ * @param {Object} logEntry a LogEntry class instance
+ * @param {string} APIURL an alternate API URL to send the log entry to
+ */
 const makeRequest = (logEntry, APIURL) => {
   const url = APIURL || `${this.baseAPIURL}/${this.loggingModuleURL}/`;
   const options = {
@@ -13,16 +18,39 @@ const makeRequest = (logEntry, APIURL) => {
       console.log(`Failed to send logEntry to server: ${e.message}`);
     });
 };
+
+/**
+ * Utility class providing server-side and local logging functionality
+ */
 export default class Logger {
+  /**
+   * Create an instance which will enable sending log messages to the server and/or the console
+   * @param {Object} options The options for the Logger instance.
+   * @param {boolean} options.logToServer Whether the Logger should send the message to a server
+   * @param {boolean} options.logToConsole Whether the message should be sent to the console
+   * @param {string} options.APIURL An API endpoint that the message should be sent to instead of the default
+   */
   constructor(options = {}) {
     this.logToServer = 'logToServer' in options ? options.logToServer : true;
     this.logToConsole = 'logToConsole' in options ? options.logToConsole : true;
+    this.APIURL = options.APIURL;
   }
 
+  /**
+   * Object containing all the valid log levels which can be used when logging messages.
+   * @readonly
+   * @memberof Logger
+   */
   get logLevels() {
     return LOG_LEVELS;
   }
 
+  /**
+   * Log a message to the destinations configured for this class instance - either to the console, the server, or both
+   * @param {string} what The message to log
+   * @param {LogLevel} [level=LOG_LEVELS.LOG] The type of log to create. Valid values are enumerated in the logLevels property
+   * @memberof Logger
+   */
   log(what, level = LOG_LEVELS.LOG) {
     const logEntry = new LogEntry(what, level);
     if (this.logToServer) {
